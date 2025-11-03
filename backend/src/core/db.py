5e1f4913c354b9ae32eb536f1afaf0ceba136342
andreym1234 <andreimalahovskii8@gmail.com>
2025-11-04 02:11:32 +0400

    fix core/config.py and core/db.py

--- backend/src/core/db.py
+++ backend/src/core/db.py
@@ -1,21 +1,8 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine
 from src.core.config import settings
 
 engine = create_async_engine(
-    settings.database_url,  # postgresql+asyncpg://...
+    settings.database_url,  # Используем URL подключения из настроек
     future=True,
     pool_pre_ping=True,
 )
-
-AsyncSessionLocal: async_sessionmaker[AsyncSession] = async_sessionmaker(
-    bind=engine, autoflush=False, expire_on_commit=False
-)
-
-class Base(DeclarativeBase):
-    pass
-
-# зависимость под fastapi/или просто convenience
-async def get_db():
-    async with AsyncSessionLocal() as session:
-        yield session
