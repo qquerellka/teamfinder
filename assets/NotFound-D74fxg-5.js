ea71e6b6bca662180d899e84f25b3921a36c3c69
qquerellka <uskovmaksof@gmail.com>
2025-11-03 23:27:41 +0300

    Updates

--- assets/NotFound-D74fxg-5.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-Ce4q5plM.js";const a=()=>s.jsx("div",{className:"",children:"No page"});export{a as NotFound,a as default};
---
+++ assets/STitle-BF4Qza-M.js
@@ -0,0 +1,14 @@
+import{n as e,d as i,m as t}from"./index-1WYqyCOz.js";const n=i(t)`
+  && {
+    ${({$fs:i})=>(i=>{if(void 0!==i)return"number"==typeof i?e`
+      font-size: ${i}px;
+      line-height: ${i+4}px;
+    `:e`
+    font-size: ${i};
+    line-height: calc(${i} + 4px);
+  `})(i)}
+    ${({$fw:i})=>void 0!==i&&e`
+        font-weight: ${i};
+      `}
+  }
+`;export{n as S};
