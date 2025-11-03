254d6918b768f887109951ea9ec5ac9e859ccfbe
andreym1234 <andreimalahovskii8@gmail.com>
2025-11-03 23:08:04 +0400

    schemas/telegram_init.py

--- backend/src/schemas/telegram_init.py
+++ backend/src/schemas/telegram_init.py
@@ -6,18 +6,23 @@ from typing import Optional, List
 # UserInitData будет использоваться для валидации и сериализации данных пользователя при инициализации
 class UserInitData(BaseModel):
     telegram_id: int
-    first_name: str
     # может быть строкой или None(значение по умолчанию)
-    last_name: Optional[str] = None
     username: Optional[str] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
     language_code: Optional[str] = None
     avatar_url: Optional[str] = None
+
+    # поля, вводимые вручную
     bio: Optional[str] = None
     age: Optional[int] = None
     city: Optional[str] = None
     university: Optional[str] = None
     link: Optional[str] = None
     skills: Optional[List[str]] = []
+
+    created_at: Optional[str] = None
+    updated_at: Optional[str] = None
 
     # Конфигурационный класс для Pydantic модели
     # orm_mode = True - позволяет модели работать с ORM объектами
