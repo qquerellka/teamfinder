a453e34ad69069de50f4a5b97d7f7daaa7b0ecdb
qquerellka <uskovmaksof@gmail.com>
2025-11-04 17:59:26 +0300

    Updates

--- assets/HackathonsPage-DfIriqv4.js
+++
@@ -1,9 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/HackathonPage-CCjLIKcr.js","assets/index-lwkJXpzC.js","assets/index-D-bDv5Dk.css","assets/HackathonCard-odCCRJNb.js","assets/STitle-B1FrL80z.js"])))=>i.map(i=>d[i]);
-import{j as a,_ as o,p as t,d as s,L as i}from"./index-lwkJXpzC.js";import{h as r,H as e}from"./HackathonCard-odCCRJNb.js";import"./STitle-B1FrL80z.js";const n=()=>a.jsx(d,{children:r.map(s=>a.jsx(p,{to:t.hackathon(s.id),onMouseEnter:()=>o(()=>import("./HackathonPage-CCjLIKcr.js"),__vite__mapDeps([0,1,2,3,4])),children:a.jsx(e,{hackathon:s,type:"part"},s.id)},s.id))}),d=s.section`
-  display: grid;
-  gap: 12px;
-`,p=s(i)`
-  display: block;
-  text-decoration: none;
-  color: inherit;
-`;export{n as HackathonsPage,n as default};
