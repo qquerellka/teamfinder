9bc89a4ed1f837ece9d3ac523fd19d1dc1a0cc53
Андрей Проскурин <andrey@MacBook-Air-Andrej.local>
2025-11-04 13:58:03 +0300

    skills sorted

--- backend/sql/initdb/06_seed_dicts.sql
+++ backend/sql/initdb/06_seed_dicts.sql
@@ -1,33 +1,48 @@
 INSERT INTO role_dict(name) VALUES
   ('DevOps'), ('Game developer'), 
-  ('IOS / Android dev'), ('Product manager'), ('DS'), ('ML'),
-   ('Fullstack'),
+  ('IOS / Android dev'), ('Product manager'), ('DS'), ('ML'), ('Fullstack'),
   ('backend'), ('frontend'), ('designer'), ('analytics'), ('qa')
 ON CONFLICT (name) DO NOTHING;
 
+-- DevOps
 INSERT INTO skill_dict(name) VALUES
-  ('python'),('go'),('java'),('kotlin'),('swift'),('dart'),
-  ('javascript'),('typescript'),('c'),('c++'),('c#'),('php'),('ruby'),('rust'),
-  ('html'),('css'),('sass'),('tailwind'),('bootstrap'),
-  ('react'),('nextjs'),('redux'),('vue'),('nuxt'),('angular'),('svelte'),
-  ('nodejs'),('express'),('nestjs'),
-  ('spring'),('spring-boot'),('django'),('flask'),('fastapi'),('.net'),('laravel'),('rails'),
-  ('android'),('ios'),('swiftui'),('react-native'),('flutter'),
-  ('sql'),('postgres'),('mysql'),('mariadb'),('sqlite'),
-  ('mongodb'),('redis'),('clickhouse'),('elasticsearch'),
-  ('kafka'),('rabbitmq'),
-  ('airflow'),('dbt'),('spark'),('hadoop'),('hive'),
-  ('tableau'),('powerbi'),('metabase'),('superset'),('excel'),
-  ('numpy'),('pandas'),('scikit-learn'),('pytorch'),('tensorflow'),
-  ('xgboost'),('catboost'),('jupyter'),('mlflow'),
   ('linux'),('bash'),('git'),
   ('docker'),('kubernetes'),('helm'),
   ('terraform'),('ansible'),
   ('nginx'),('grafana'),('prometheus'),
   ('github-actions'),('gitlab-ci'),
-  ('aws'),('gcp'),('azure'),
-  ('pytest'),('unittest'),('junit'),
+  ('aws'),('gcp'),('azure');
+
+-- Backend
+INSERT INTO skill_dict(name) VALUES
+  ('python'),('go'),('java'),('kotlin'),('swift'),('dart'),
+  ('c'),('c++'),('c#'),('php'),('ruby'),('rust'),
+  ('nodejs'),('express'),('nestjs'),
+  ('spring'),('spring-boot'),('django'),('flask'),('fastapi'),('.net'),('laravel'),('rails'),
+  ('mongodb'),('redis'),('kafka'),('rabbitmq'),
+  ('postgres'),('mysql'),('mariadb'),('sqlite'),
+  ('clickhouse'),('elasticsearch'),
+  ('airflow'),('dbt'),('spark'),('hadoop'),('hive');
+
+-- Frontend
+INSERT INTO skill_dict(name) VALUES
+  ('javascript'),('typescript'),
+  ('html'),('css'),('sass'),('tailwind'),('bootstrap'),
+  ('react'),('nextjs'),('redux'),('vue'),('nuxt'),('angular'),('svelte'),
+  ('android'),('ios'),('swiftui'),('react-native'),('flutter'),
   ('selenium'),('cypress'),('playwright'),
-  ('figma'),('adobe-xd'),('illustrator'),
-  ('amplitude'),('mixpanel'),('ga4')
+  ('figma'),('adobe-xd'),('illustrator');
+
+-- Data Science / ML
+INSERT INTO skill_dict(name) VALUES
+  ('sql'),
+  ('numpy'),('pandas'),('scikit-learn'),('pytorch'),('tensorflow'),
+  ('xgboost'),('catboost'),('jupyter'),('mlflow'),
+  ('tableau'),('powerbi'),('metabase'),('superset'),('excel'),
+  ('amplitude'),('mixpanel'),('ga4');
+
+-- Testing
+INSERT INTO skill_dict(name) VALUES
+  ('pytest'),('unittest'),('junit');
+
 ON CONFLICT (name) DO NOTHING;
