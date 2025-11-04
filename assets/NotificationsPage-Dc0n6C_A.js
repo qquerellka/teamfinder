517f1bb1e57405ba6f3c9cc1f9d3193aa6d54b91
qquerellka <uskovmaksof@gmail.com>
2025-11-04 22:14:59 +0300

    Updates

--- assets/NotificationsPage-Dc0n6C_A.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-BgRDkMUf.js";import{A as s}from"./Avatar-D2T5ayWy.js";const e=()=>a.jsx(s,{src:"/teamfinder/assets/hackathonImage7-DVO5CaH5.webp",size:96});export{e as NotificationsPage,e as default};
---
+++ assets/UserTeamsPage-GHO_ahVk.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-pJ0R_zlF.js";const a=()=>s.jsx("div",{className:"",children:"Мои команды"});export{a as UserTeamsPage,a as default};
