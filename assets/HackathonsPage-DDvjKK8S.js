d21ae8e44fbc77382c3ffdf180a8e6efff06bb21
qquerellka <uskovmaksof@gmail.com>
2025-11-04 14:01:41 +0300

    Updates

--- assets/HackathonsPage-DDvjKK8S.js
+++
@@ -1,9 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/HackathonPage-COF3Ugo-.js","assets/index-hAlDAkbk.js","assets/index-D-bDv5Dk.css","assets/HackathonCard-HuvulSlD.js","assets/STitle-aKWjFuaH.js"])))=>i.map(i=>d[i]);
-import{j as a,_ as o,p as t,d as s,L as i}from"./index-hAlDAkbk.js";import{h as r,H as e}from"./HackathonCard-HuvulSlD.js";import"./STitle-aKWjFuaH.js";const n=()=>a.jsx(d,{children:r.map(s=>a.jsx(p,{to:t.hackathon(s.id),onMouseEnter:()=>o(()=>import("./HackathonPage-COF3Ugo-.js"),__vite__mapDeps([0,1,2,3,4])),children:a.jsx(e,{hackathon:s,type:"part"},s.id)},s.id))}),d=s.section`
-  display: grid;
-  gap: 12px;
-`,p=s(i)`
-  display: block;
-  text-decoration: none;
-  color: inherit;
-`;export{n as HackathonsPage,n as default};
---
+++ assets/NotFound-l2BGh932.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-lwkJXpzC.js";const a=()=>s.jsx("div",{className:"",children:"No page"});export{a as NotFound,a as default};
