686d6e4fb55da2c70d9b2789fc5019d4934ec22b
qquerellka <uskovmaksof@gmail.com>
2025-11-04 22:38:59 +0300

    Updates

--- assets/HackathonsPage-BKnvLaXf.js
+++
@@ -1,9 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/HackathonPage-aFSWEgo_.js","assets/index-CefgiUGe.js","assets/index-D-bDv5Dk.css","assets/HackathonCard-CYaLr33W.js","assets/STitle-F8_QGGCM.js"])))=>i.map(i=>d[i]);
-import{j as a,_ as o,p as t,d as s,L as i}from"./index-CefgiUGe.js";import{h as r,H as e}from"./HackathonCard-CYaLr33W.js";import"./STitle-F8_QGGCM.js";const n=()=>a.jsx(d,{children:r.map(s=>a.jsx(p,{to:t.hackathon(s.id),onMouseEnter:()=>o(()=>import("./HackathonPage-aFSWEgo_.js"),__vite__mapDeps([0,1,2,3,4])),children:a.jsx(e,{hackathon:s,type:"part"},s.id)},s.id))}),d=s.section`
-  display: grid;
-  gap: 12px;
-`,p=s(i)`
-  display: block;
-  text-decoration: none;
-  color: inherit;
-`;export{n as HackathonsPage,n as default};
---
+++ assets/HackathonsPage-sea20sU2.js
@@ -0,0 +1,9 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/HackathonPage-fIYTFdFp.js","assets/index-D_EYXGky.js","assets/index-D-bDv5Dk.css","assets/HackathonCard-D7dMZ0KE.js","assets/STitle-DCZnFqhk.js"])))=>i.map(i=>d[i]);
+import{j as a,_ as o,p as t,d as s,L as i}from"./index-D_EYXGky.js";import{h as r,H as e}from"./HackathonCard-D7dMZ0KE.js";import"./STitle-DCZnFqhk.js";const n=()=>a.jsx(d,{children:r.map(s=>a.jsx(p,{to:t.hackathon(s.id),onMouseEnter:()=>o(()=>import("./HackathonPage-fIYTFdFp.js"),__vite__mapDeps([0,1,2,3,4])),children:a.jsx(e,{hackathon:s,type:"part"},s.id)},s.id))}),d=s.section`
+  display: grid;
+  gap: 12px;
+`,p=s(i)`
+  display: block;
+  text-decoration: none;
+  color: inherit;
+`;export{n as HackathonsPage,n as default};
