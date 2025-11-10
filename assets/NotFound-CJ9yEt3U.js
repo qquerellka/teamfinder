9d41f23548e8309d932da323d1577922b3144b36
qquerellka <uskovmaksof@gmail.com>
2025-11-10 23:11:06 +0300

    Updates

--- assets/NotFound-CJ9yEt3U.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-Dxn1mkw0.js";const a=()=>s.jsx("div",{className:"",children:"No page"});export{a as NotFound,a as default};
--- assets/UserTeamsPage-CihtZKrn.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-Dxn1mkw0.js";const a=()=>s.jsx("div",{className:"",children:"Мои команды"});export{a as UserTeamsPage,a as default};
