a9adbdabef8b9165732a22f7a371236a33b6fbc2
qquerellka <uskovmaksof@gmail.com>
2025-11-11 12:54:18 +0300

    Updates

--- assets/NotificationsPage-gZ3AIvTR.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-nTbKghKU.js";import{A as s}from"./Avatar-_MJuParx.js";import"./Subheadline-BGPasnHk.js";const e=()=>a.jsx(s,{src:"/teamfinder/assets/hackathonImage7-DVO5CaH5.webp",size:96});export{e as NotificationsPage,e as default};
