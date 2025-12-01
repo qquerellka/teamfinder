9b86191b7cba77a5769db69c1189bacd49bc32f2
qquerellka <uskovmaksof@gmail.com>
2025-12-02 00:30:32 +0300

    Updates

--- assets/Page-BOOy0UF_.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-_TVuRRT6.js";import{A as s}from"./Avatar-NwuhlW5D.js";import"./Subheadline-BnYcqGz_.js";const e=()=>a.jsx(s,{src:"/teamfinder/assets/hackathonImage7-DVO5CaH5.webp",size:96});export{e as NotificationsPage,e as default};
---
+++ assets/Page-YmyHFTKV.js
@@ -0,0 +1,11 @@
+import{j as i,p as s,d as a,L as o}from"./index-DY60WZzM.js";import{u as e,H as n}from"./hackathon-api-Bui5RWqH.js";import"./client-DYgysrB0.js";import"./Subheadline-BJxiZb6j.js";const t=()=>{const{data:a,isLoading:o,error:t}=e();return o?i.jsx("div",{children:"Загружаем хакатоны…"}):t?i.jsx("div",{children:"Что-то пошло не так"}):a?.items.length?(console.log(a),i.jsx(d,{children:a.items.map(a=>i.jsx(r,{to:s.hackathon(a.id),children:i.jsx(n,{hackathon:a})},a.id))})):i.jsx("div",{children:"Пока нет активных хакатонов"})},d=a.section`
+  display: grid;
+  gap: 12px;
+`,r=a(o)`
+  display: block;
+  text-decoration: none;
+  color: inherit;
+`,c=()=>i.jsx(l,{children:i.jsx(t,{})}),l=a.section`
+  display: grid;
+  gap: 12px;
+`;export{c as HackathonsPage,c as default};
